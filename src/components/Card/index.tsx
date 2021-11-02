cf419bd61c7e33ca0401dd39845650a1aab3c0b2
Furkan Topaloğlu <furkantopaloglu@Furkans-MacBook-Pro.local>
2021-11-03 00:12:40 +0300

    add memoization card component for optimization

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,11 +6,11 @@ interface ICardProps{
     title: string;
 }
 
-export const Card: FC<ICardProps> = ({image, title}) => {
+export const Card: FC<ICardProps> = React.memo(({image, title}) => {
     return (
         <div className="card">
             <div className="card-image"><img src={image} alt={title} /></div>
             <div className="card-title">{title}</div>
         </div>
     )
-}
+})
